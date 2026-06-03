e10a4f4a8b6676145f377099e0fdcf0b91139838
KlosheKopa <richardsa@my.lanecc.edu>
2026-06-03 11:49:24 -0700

    final rough product

--- Assets/Scripts/Menus/SceneSelector.cs
+++ Assets/Scripts/Menus/SceneSelector.cs
@@ -3,9 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    [Header("Data Reset")]
+    [Tooltip("Drag your ActivePlayerRunData ScriptableObject file here to wipe it on a new run")]
+    public PlayerStatData runDataToClear;
+
     public void LoadScene(int sceneID)
     {
         Time.timeScale = 1f;
+
+        // FIXED: Wipes the upgrades and level/EXP data right before loading the scene
+        if (runDataToClear != null)
+        {
+            runDataToClear.ResetToDefaults();
+        }
 
         SceneManager.LoadScene(sceneID);
     }
